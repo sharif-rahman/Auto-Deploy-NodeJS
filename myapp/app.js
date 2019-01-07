722f0126add00e8a2249af3486c46601aaa7e03c
root <root@localhost.localdomain>
2019-01-07 05:49:32 -0500

    new file:   DockerfilesInfo/Dockerfile-NODEJS 	new file:   DockerfilesInfo/Dockerfile-jenkinsIMG 	new file:   ReadMe 	new file:   myapp/Dockerfile 	new file:   myapp/app.js 	new file:   myapp/docker-compose.yml 	new file:   myapp/main.sh 	new file:   myapp/package.json 	new file:   myapp/views/LICENSE 	new file:   myapp/views/README.md 	new file:   myapp/views/assets/favicons/favicon114.png 	new file:   myapp/views/assets/favicons/favicon120.png 	new file:   myapp/views/assets/favicons/favicon144.png 	new file:   myapp/views/assets/favicons/favicon152.png 	new file:   myapp/views/assets/favicons/favicon16.png 	new file:   myapp/views/assets/favicons/favicon180.png 	new file:   myapp/views/assets/favicons/favicon192.png 	new file:   myapp/views/assets/favicons/favicon32.png 	new file:   myapp/views/assets/favicons/favicon57.png 	new file:   myapp/views/assets/favicons/favicon60.png 	new file:   myapp/views/assets/favicons/favicon72.png 	new file:   myapp/views/assets/favicons/favicon76.png 	new file:   myapp/views/assets/favicons/favicon96.png 	new file:   myapp/views/assets/icons/building.png 	new file:   myapp/views/assets/icons/building.svg 	new file:   myapp/views/assets/icons/contact.png 	new file:   myapp/views/assets/icons/contact.svg 	new file:   myapp/views/assets/icons/cooking.png 	new file:   myapp/views/assets/icons/cooking.svg 	new file:   myapp/views/assets/icons/decoration.png 	new file:   myapp/views/assets/icons/decoration.svg 	new file:   myapp/views/assets/icons/info.png 	new file:   myapp/views/assets/icons/info.svg 	new file:   myapp/views/assets/icons/listening.png 	new file:   myapp/views/assets/icons/listening.svg 	new file:   myapp/views/assets/icons/mail.png 	new file:   myapp/views/assets/icons/mail.svg 	new file:   myapp/views/assets/icons/objective.png 	new file:   myapp/views/assets/icons/objective.svg 	new file:   myapp/views/assets/icons/phone.png 	new file:   myapp/views/assets/icons/phone.svg 	new file:   myapp/views/assets/icons/travelling.png 	new file:   myapp/views/assets/icons/travelling.svg 	new file:   myapp/views/assets/images/person.png 	new file:   myapp/views/css/basic.css 	new file:   myapp/views/index.html 	new file:   myapp/views/js/code.js

---
+++ myapp/app.js
@@ -0,0 +1,25 @@
+
+var express = require("express");
+var app = express();
+var router = express.Router();
+
+var path = __dirname + '/views/';
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+
+app.use(express.static(path));
+app.use("/", router);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
---
+++ myapp/views/README.md
@@ -0,0 +1,17 @@
+# Resume template layouting
+
+Create the layout of the resume template and add some interactive to it
+
+Technologies and tools:
+
+- Photoshop (working with the template)
+- HTML
+  - Semantic
+  - Accessibility
+  - Responsive
+- CSS
+  - Bootstrap 4
+- Javascript
+  - ES6
+
+![Resume Template](https://github.com/OceanJS-JOB/Resume-template/blob/master/resume.png)
