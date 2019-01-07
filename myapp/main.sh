0eb9176fd98470129a16c85d4c7657a98321fa85
sharif-rahman <45973680+sharif-rahman@users.noreply.github.com>
2019-01-07 15:34:09 +0100

    Update main.sh

--- myapp/main.sh
+++ myapp/main.sh
@@ -7,8 +7,9 @@ BUILDIMAGE="$(docker-compose -f $WORKSPACE/myapp/docker-compose.yml up -d)"
 #BUILDIMAGE="$(docker-compose up -d)"
 REGISTRY=shariftest
 #LOCALIMAGE=$(sudo docker-compose images | awk '$1 ~ /shariftest/ { print $2 }')
-LOCALIMAGE=$(docker-compose images|awk '$1~/shariftest/{ print $2 }')
-LOCAL_TAG=$(sudo docker-compose images | awk '$1 ~ /shariftest/ { print $3 }')
+LOCALIMAGE=myapp_app
+#LOCAL_TAG=$(sudo docker-compose images | awk '$1 ~ /shariftest/ { print $3 }')
+LOCAL_TAG=latest
 REMOTE_IMAGE=test
 REMOTE_TAG=04
 
